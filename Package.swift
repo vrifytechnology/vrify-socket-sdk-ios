f56aa9984b744e1c6f9c17e46f2db7e5fb2583e4
Jatinder Sidhu <jatinder@vrify.com>
2022-09-28 14:46:34 -0700

    Update Package.swift
    Updates SPM platforms to those that support concurrency

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftPhoenixClient",
     platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10),
-        .watchOS(.v3)
+        .macOS("12.0"),
+        .iOS("13.0"),
+        .tvOS("15.0"),
+        .watchOS("8.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
